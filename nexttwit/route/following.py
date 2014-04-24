ba4ba66397dd65b878ecd5c39683b2a4c444c154
eternity <snatcher@nhn.com>
2014-04-24 19:55:59 +0900

    follower 로직

--- nexttwit/route/following.py
+++ nexttwit/route/following.py
@@ -7,17 +7,17 @@ from server import app
 from dbgateway import database 
 
 # 여기에 원하는 주소를 입력하세요
-@app.route('/follow', methods=['POST'])
+@app.route('/', methods=['POST'])
 def follow_user():
     if not g.user:
         abort(401)
     
     # userid를 이용해 사용자 정보를 읽어 오도록 수정하세요
-    whom = database.findUser(request.form['userid'])
+    whom = database.findUser('#여기를 수정하시면 됩니다')
     follower = Follower(g.user, whom)
     
     # database.saveFollower()를 이용해 follower를 저장하세요
-    database.saveFollower(follower)
+    
     
     return redirect(url_for('user_timeline', userid=request.form['userid']))
 
